d206d0caeaa558c7781239b0ad92223ce8c4489c
pmall <pierre.mallinjoud@gmail.com>
2019-12-23 20:19:56 +0100

    better interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 import genetic, { Map, Population } from 'genetic'
 import { MapSvg } from 'components/MapSvg';
 
 const init = { w: 500, h: 500, x: 40, n: 100, i: 10 }
 
-const nextn = (population: Population, i: number): Promise<Population> => {
-    return new Promise((resolve) => setTimeout(() => {
-        let tmp = population
-        for (let j = 0; j < i; j++) tmp = genetic.next(tmp)
-        resolve(tmp)
-    }, 100))
-}
-
-const App: React.FC = () => {
-    const [w, setW] = useState<number>(init.w)
-    const [h, setH] = useState<number>(init.h)
+export const App: React.FC = () => {
     const [x, setX] = useState<number>(init.x)
     const [n, setN] = useState<number>(init.n)
     const [i, setI] = useState<number>(init.i)
@@ -24,37 +14,36 @@ const App: React.FC = () => {
     const [processing, setProcessing] = useState<boolean>(false)
 
     const mapIsValid = true
-        && w >= 100 && w <= 1000
-        && h >= 100 && h <= 1000
+        && init.w >= 100 && init.w <= 1000
+        && init.h >= 100 && init.h <= 1000
         && x >= 10 && x <= 100
+        && n >= 10 && n <= 100
 
-    const populationIsValid = mapIsValid && n >= 10 && n <= 100
-
-    const generationIsValid = populationIsValid && i >= 1 && i <= 1000
-
-    useEffect(() => {
-        if (mapIsValid) setMap(genetic.map(w, h, x))
-    }, [mapIsValid, w, h, x])
-
-    useEffect(() => {
-        if (populationIsValid) setPopulation(genetic.population(map, n))
-    }, [populationIsValid, map, n])
+    const generationIsValid = i >= 1 && i <= 1000
 
     const newMap = () => {
-        if (mapIsValid) setMap(genetic.map(h, w, x))
+        if (!mapIsValid) return
+        const map = genetic.map(init.w, init.h, x);
+        const population = genetic.population(map, n);
+        setMap(map)
+        setPopulation(population)
     }
 
-    const newPopulation = () => {
-        if (populationIsValid) setPopulation(genetic.population(map, n))
-    }
-
-    const newGeneration = () => {
+    const newGeneration = async () => {
         if (!generationIsValid) return;
+
         setProcessing(true)
-        nextn(population, i).then(population => {
-            setPopulation(population)
-            setProcessing(false)
-        })
+
+        const newGeneration = await new Promise<Population>(resolve => setTimeout(() => {
+            let tmp = population
+            for (let j = 0; j < i; j++) {
+                tmp = genetic.next(tmp)
+            }
+            resolve(tmp)
+        }, 0))
+
+        setPopulation(newGeneration)
+        setProcessing(false)
     }
 
     return (
@@ -63,33 +52,33 @@ const App: React.FC = () => {
                 <div className="col-4">
                     <form onSubmit={e => e.preventDefault()}>
                         <fieldset>
-                            <legend>Map</legend>
+                            <legend>Generation</legend>
                             <div className="form-group">
-                                <label>Width:</label>
+                                <label>Nb iterations</label>
                                 <input
                                     type="number"
-                                    min="100"
+                                    min="1"
                                     max="1000"
-                                    value={w}
+                                    value={i.toString()}
                                     disabled={processing}
                                     className="form-control"
-                                    onChange={e => setW(parseInt(e.target.value))}
+                                    onChange={(e) => setI(parseInt(e.target.value))}
                                 />
                             </div>
+                            <button
+                                type="button"
+                                className="btn btn-block btn-primary"
+                                onClick={newGeneration}
+                                disabled={processing || !generationIsValid}
+                            >
+                                New generation
+                            </button>
+                        </fieldset>
+                        <hr />
+                        <fieldset>
+                            <legend>Instance</legend>
                             <div className="form-group">
-                                <label>Height:</label>
-                                <input
-                                    type="number"
-                                    min="100"
-                                    max="1000"
-                                    value={h.toString()}
-                                    disabled={processing}
-                                    className="form-control"
-                                    onChange={(e) => setH(parseInt(e.target.value))}
-                                />
-                            </div>
-                            <div className="form-group">
-                                <label>NB cities:</label>
+                                <label>NB cities</label>
                                 <input
                                     type="number"
                                     min="10"
@@ -100,18 +89,6 @@ const App: React.FC = () => {
                                     onChange={(e) => setX(parseInt(e.target.value))}
                                 />
                             </div>
-                            <button
-                                type="button"
-                                className="btn btn-block btn-primary"
-                                onClick={newMap}
-                                disabled={processing || !mapIsValid}
-                            >
-                                New map
-                            </button>
-                        </fieldset>
-                        <hr />
-                        <fieldset>
-                            <legend>Population</legend>
                             <div className="form-group">
                                 <label>Nb individuals</label>
                                 <input
@@ -128,50 +105,28 @@ const App: React.FC = () => {
                             <button
                                 type="button"
                                 className="btn btn-block btn-primary"
-                                onClick={newPopulation}
-                                disabled={processing || !populationIsValid}
+                                onClick={newMap}
+                                disabled={processing || !mapIsValid}
                             >
-                                New population
+                                New map
                             </button>
                         </fieldset>
-                        <hr />
-                        <fieldset>
-                            <legend>Generation</legend>
-                            <div className="form-group">
-                                <label>Nb iterations</label>
-                                <input
-                                    type="number"
-                                    min="1"
-                                    max="1000"
-                                    value={i.toString()}
-                                    className="form-control"
-                                    onChange={(e) => setI(parseInt(e.target.value))}
-                                />
-                            </div>
-                            <button
-                                type="button"
-                                className="btn btn-block btn-primary"
-                                onClick={newGeneration}
-                                disabled={processing || !generationIsValid}
-                            >
-                                New generation
-                            </button>
-                        </fieldset>
-                        <hr />
-                        <p>
+                    </form>
+                </div>
+                <div className="col">
+                    <div className="card">
+                        <div className="card-header">
                             {processing
                                 ? 'Processing...'
                                 : `Distance: ${genetic.fitness(population[0]).toFixed(0)}`
                             }
-                        </p>
-                    </form>
-                </div>
-                <div className="col">
-                    <MapSvg w={w} h={h} map={population[0]} />
+                        </div>
+                        <div className="card-body">
+                            <MapSvg w={init.w} h={init.h} map={population[0]} />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     );
 }
-
-export default App;
